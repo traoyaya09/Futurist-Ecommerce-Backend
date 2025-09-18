fc3d8caa71605af8a7da78a7aa1ddb7d256c395a
traoyaya09 <traoyaya09@gmail.com>
2025-09-18 07:01:42 +0100

    update

--- routes/TransactionRoute.js
+++ routes/TransactionRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const transactionController = require('../controllers/transactionController');
+const transactionController = require('../controllers/TransactionController');
 const { authenticate } = require('../middleware/authentication');
 const { validateTransactionCreation, validateTransactionUpdate } = require('../validators/transactionValidator');
 
